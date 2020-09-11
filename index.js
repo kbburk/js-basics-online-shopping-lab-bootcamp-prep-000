49f4c8aa032a2c319b70d6e2ed68d584396b69c7
Kimberly Sampson <kbburk54@yahoo.com>
2020-09-11 08:00:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -83,9 +83,10 @@ function placeOrder(cardNumber) {
   if (!cardNumber){
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    const total1 = total()
+   // const total1 = total()
     cart = []
-    return `Your total cost is $${total1}, which will be charged to the card ${cardNumber}.`
+   // return `Your total cost is $${total1}, which will be charged to the card ${cardNumber}.`
+   retuan `your total cost is $${total()}, which will be charged to the card ${cardnumber}.`
   }
 }
 
