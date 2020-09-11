8b42a3aa12e6e883702c8cde5be709cc79731daa
Kimberly Sampson <kbburk54@yahoo.com>
2020-09-11 09:04:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -80,6 +80,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  let yourTotal = 
   if (!cardNumber){
     return "Sorry, we don't have a credit card on file for you."
   } else {
